97b6dda75a57fcaca93578d6c914c61e766eafd5
tomocy <tomocy.dev@gmail.com>
2019-07-03 10:26:10 +0900

    move things to more proper file

--- client/client.go
+++ client/client.go
@@ -17,3 +17,20 @@ type runner interface {
 func (c *Client) Run(args []string) error {
 	return c.runner.Run(args)
 }
+
+func newCreater(os string) creater {
+	switch os {
+	case osLinux:
+		return new(linux)
+	default:
+		return nil
+	}
+}
+
+type creater interface {
+	create(id string) error
+}
+
+const (
+	osLinux = "linux"
+)
--- client/client_linux.go
+++ client/client_linux.go
@@ -4,23 +4,6 @@ import (
 	"github.com/tomocy/kibidango/factory"
 )
 
-func newCreater(os string) creater {
-	switch os {
-	case osLinux:
-		return new(linux)
-	default:
-		return nil
-	}
-}
-
-type creater interface {
-	create(id string) error
-}
-
-const (
-	osLinux = "linux"
-)
-
 type linux struct{}
 
 func (l *linux) create(id string) error {
