ef5bf39cef5d23dc94ff723f6f98d71332054783
tomocy <tomocy.dev@gmail.com>
2019-07-04 05:15:21 +0900

    add methods to load spec in oci format

---
+++ spec/oci.go
@@ -0,0 +1,34 @@
+package spec
+
+import (
+	"encoding/json"
+	"os"
+
+	"github.com/opencontainers/runtime-spec/specs-go"
+	"github.com/tomocy/kibidango"
+)
+
+type OCI struct{}
+
+func (o *OCI) Load(name string) (*Spec, error) {
+	src, err := os.Open(name)
+	if err != nil {
+		return nil, err
+	}
+	defer src.Close()
+
+	var ociSpec *specs.Spec
+	if err := json.NewDecoder(src).Decode(&ociSpec); err != nil {
+		return nil, err
+	}
+
+	return o.adapt(ociSpec), nil
+}
+
+func (o *OCI) adapt(ociSpec *specs.Spec) *Spec {
+	return &Spec{
+		Process: &kibidango.Process{
+			Args: ociSpec.Process.Args,
+		},
+	}
+}
